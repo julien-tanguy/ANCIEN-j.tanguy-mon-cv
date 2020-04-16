05e320a30a6b420fa3690f0ebe22c29a81109de1
julien tanguy <j.tanguy24@gmail.com>
2020-04-16 10:01:51 +0200

    font-size a changer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    formationPremier();
+  });
+
 function competencePremier() {
-    document.getElementById("competenceContent").style.zIndex = "3";
-    document.getElementById("experienceContent").style.zIndex = "2";
-    document.getElementById("formationContent").style.zIndex = "1";
+    document.getElementById("formationContent").style.display = "none";
+    document.getElementById("experienceContent").style.display = "none";
+    document.getElementById("competenceContent").style.display = "block";
 }
 
 function experiencePremier() {
-    document.getElementById("competenceContent").style.zIndex = "2";
-    document.getElementById("experienceContent").style.zIndex = "3";
-    document.getElementById("formationContent").style.zIndex = "1";
+    document.getElementById("formationContent").style.display = "none";
+    document.getElementById("experienceContent").style.display = "block";
+    document.getElementById("competenceContent").style.display = "none";
 }
 
 function formationPremier() {
-    document.getElementById("competenceContent").style.zIndex = "1";
-    document.getElementById("experienceContent").style.zIndex = "2";
-    document.getElementById("formationContent").style.zIndex = "3";
+    document.getElementById("formationContent").style.display = "block";
+    document.getElementById("experienceContent").style.display = "none";
+    document.getElementById("competenceContent").style.display = "none";
 }
+
+/*document.getElementById("competenceContent").style.zIndex = "1";
+    document.getElementById("experienceContent").style.zIndex = "2";
+    document.getElementById("formationContent").style.zIndex = "3";*/
